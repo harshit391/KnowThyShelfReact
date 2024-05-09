d178f0076c90f6bb53a90f14e3e3941ecbfef9f0
Harshit Singla <harshitsingla1761@gmail.com>
2024-05-09 20:00:49 +0530

    Routing Fix

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 // import '../public/script.js'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <Router>
+  <BrowserRouter basename='/'>
     <App />
-  </Router>,
-  document.getElementById('root')
+  </BrowserRouter>,
 )
