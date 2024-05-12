6578af9fbeb0d2055db40a920e0808241a47989c
Harshit Singla <harshitsingla1761@gmail.com>
2024-05-12 23:27:11 +0530

    Premium and Testimonials Fixed

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,7 +4,6 @@ import Main from './Main'
 import ChooseYourPick from './ChooseYourPick'
 import HowKnowThyShelfWorks from './HowKnowThyShelfWorks'
 import '../App.css'
-import Test from './Testimonials'
 
 const Landing = () => {
   return (
@@ -12,8 +11,7 @@ const Landing = () => {
         <Main/>
       <Carl/>
       <ChooseYourPick />
-      <HowKnowThyShelfWorks/>
-      <Test/>      
+      <HowKnowThyShelfWorks/>      
     </div>
   )
 }
